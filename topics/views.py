6e0cf4af80c1ef5a0a20ef130e14be9d05aa60e0
Andy <astubbs50@gmail.com>
2024-11-05 10:25:23 -0700

    Fixed bug with prompt view js not loading and made sure views not loaded directly

--- topics/views.py
+++ topics/views.py
@@ -1,8 +1,13 @@
+# topics/views.py
+from django.http import JsonResponse, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
 
 def prompt( request ):
+	if not request.headers.get( "X-Requested-View" ) == "topics:prompt":
+		return HttpResponseForbidden( "This view can only be accessed via AJAX." )
+	
 	context = {
 		"prompt_message": "What would you like to study today?"
 	}
